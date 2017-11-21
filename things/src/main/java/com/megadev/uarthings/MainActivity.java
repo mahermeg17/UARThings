9c8cbc89a974df72db92f3b7fa0d17f170cef232
mahermeg17 <mahermeg17@gmail.com>
2017-11-21 14:34:08 +0100

    basic uart activity

--- things/src/main/java/com/megadev/uarthings/MainActivity.java
+++ things/src/main/java/com/megadev/uarthings/MainActivity.java
@@ -2,6 +2,14 @@ package com.megadev.uarthings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.UartDevice;
+import com.google.android.things.pio.UartDeviceCallback;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Skeleton of an Android Things activity.
@@ -24,9 +32,92 @@ import android.os.Bundle;
  */
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
+
+    private UartDevice mDevice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PeripheralManagerService manager = new PeripheralManagerService();
+        List<String> deviceList = manager.getUartDeviceList();
+        if (deviceList.isEmpty()) {
+            Log.i(TAG, "No UART port available on this device.");
+        } else {
+            Log.i(TAG, "List of available devices: " + deviceList);
+            try {
+                mDevice = manager.openUartDevice(deviceList.get(0));
+                configureUartFrame(mDevice);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Begin listening for interrupt events
+        try {
+            mDevice.registerUartDeviceCallback(mUartCallback);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Interrupt events no longer necessary
+        mDevice.unregisterUartDeviceCallback(mUartCallback);
+    }
+
+    private UartDeviceCallback mUartCallback = new UartDeviceCallback() {
+        @Override
+        public boolean onUartDeviceDataAvailable(UartDevice uart) {
+            // Read available data from the UART device
+            try {
+                readUartBuffer(uart);
+            } catch (IOException e) {
+                Log.w(TAG, "Unable to access UART device", e);
+            }
+
+            // Continue listening for more interrupts
+            return true;
+        }
+
+        @Override
+        public void onUartDeviceError(UartDevice uart, int error) {
+            Log.w(TAG, uart + ": Error event " + error);
+        }
+    };
+
+    public void readUartBuffer(UartDevice uart) throws IOException {
+        // Maximum amount of data to read at one time
+        final int maxCount = 256;
+        byte[] buffer = new byte[maxCount];
+
+        int count;
+        while ((count = uart.read(buffer, buffer.length)) > 0) {
+            Log.i(TAG, "Read " + count + " bytes from peripheral");
+        }
+    }
+
+    public void writeUartData(UartDevice uart) throws IOException {
+        byte[] buffer = new byte[0];
+        int count = uart.write(buffer, buffer.length);
+        Log.i(TAG, "Wrote " + count + " bytes to peripheral");
+    }
+
+    public void configureUartFrame(UartDevice uart) throws IOException {
+        // Configure the UART port
+        uart.setBaudrate(115200);
+        uart.setDataSize(8);
+        uart.setParity(UartDevice.PARITY_NONE);
+        uart.setStopBits(1);
+    }
+
+
 }
